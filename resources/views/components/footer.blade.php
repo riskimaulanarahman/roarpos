fd949bee74e7762342dfcf551d5ac3b85f789532
Riski Maulana Rahman <61986465+riskimaulanarahman@users.noreply.github.com>
2025-01-01 10:08:24 +0800

    adjust : title

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="main-footer">
     <div class="footer-left">
-        Copyright &copy; {{ date('Y') }} <div class="bullet"></div> Developer by JagoFlutter
+        Copyright &copy; {{ date('Y') }} <div class="bullet"></div> Developer by Pinday
     </div>
     <div class="footer-right">
         1.0.0
--- resources/views/pages/auth/login.blade.php
+++ resources/views/pages/auth/login.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.auth')
 
-@section('title', 'Jago Flutter')
+@section('title', 'ROAR')
 
 @push('style')
     <!-- CSS Libraries -->
