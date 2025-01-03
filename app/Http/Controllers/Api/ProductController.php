10177a63a37f1b3586ba7fcb1e2ab061e6d5c9fe
Riski Maulana Rahman <61986465+riskimaulanarahman@users.noreply.github.com>
2025-01-03 08:35:21 +0800

    order by best seller

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -14,7 +14,7 @@ class ProductController extends Controller
     public function index()
     {
         //all products
-        $products = \App\Models\Product::orderBy('id', 'desc')->get();
+        $products = \App\Models\Product::orderBy('is_best_seller', 'desc')->get();
         //load category
         $products->load('category');
         return response()->json([
