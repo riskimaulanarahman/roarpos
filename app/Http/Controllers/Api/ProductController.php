faf6bb66932bbdd77662bdcac13d72dd2cf8c990
Riski Maulana Rahman <61986465+riskimaulanarahman@users.noreply.github.com>
2025-08-11 21:43:56 +0800

    112produk

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Validator;
 
 class ProductController extends Controller
 {
