cfeaa3278116a975bbb3f8acf0a33f03c0f996a8
Riski Maulana Rahman <61986465+riskimaulanarahman@users.noreply.github.com>
2025-09-09 10:23:42 +0800

    add

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\Category;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Validation\Rule;
 
 class CategoryController extends Controller
 {
